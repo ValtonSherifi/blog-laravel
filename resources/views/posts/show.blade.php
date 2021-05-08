da6f8807ab2ff42be04f6a9ba81d65b518660705
Valton <valtoni.gl@live.com>
2021-05-08 17:41:48 +0200

    Security and Authentication

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,12 +8,16 @@
         {!!$post->body!!}
     </div>
     <hr>
-    <small>Written on {{$post->created_at}}</small>
+    <small>Written on {{$post->created_at}}  by<b> {{$post->user->name}}<b></small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
+    @if(!Auth::guest())
+    @if(Auth::user()->id == $post->user_id)
+         <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
 
-    {!!Form::open(['action'=>['App\Http\Controllers\PostsController@destroy',$post->id],'method'=>'POST','class'=>'float-right'])!!}
-        {{Form::hidden('_method','DELETE')}}
-        {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
-    {!!Form::close()!!}
+         {!!Form::open(['action'=>['App\Http\Controllers\PostsController@destroy',$post->id],'method'=>'POST','class'=>'float-right'])!!}
+         {{Form::hidden('_method','DELETE')}}
+         {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+         {!!Form::close()!!}
+    @endif
+    @endif
 @endsection
